9de3537c6bc6bc69d72ef5a3b49984961de75d87
rmazza <robertmazza1@gmail.com>
2024-01-24 08:36:13 -0700

    moved socket address parsing to impl method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::net::{TcpStream, ToSocketAddrs};
+use std::net::{TcpStream, ToSocketAddrs, SocketAddr};
 use std::time::Duration;
 use rayon::prelude::*;
 use rayon::ThreadPoolBuilder;
@@ -8,8 +8,8 @@ use rayon::ThreadPoolBuilder;
 #[derive(Parser)]
 struct Cli {
     /// host ip to connect to
-    #[arg(short = 'H', long)]
-    host: String,
+    #[arg(short = 'H', long, use_value_delimiter = true, value_delimiter = ',', required = true)]
+    hosts: Vec<String>,
     /// port to check
     #[arg(short, long, default_value = "-1")]
     port: String,
@@ -19,11 +19,28 @@ struct Cli {
 }
 
 impl Cli {
-    fn url(&self) -> String {
-        format!("{}:{}", self.host, self.port)
+    fn get_socket_addrs(self) -> Vec<SocketAddr> {
+        self.hosts
+        .into_iter()
+        .flat_map(|addr| { 
+            let full_addr = format!("{}:{}", addr, self.port);
+            full_addr.to_socket_addrs().ok()
+        })
+        .flatten()
+        .collect::<Vec<SocketAddr>>()
+    }
+
+    fn get_socket_addrs_with_port(&self, port: i32) -> Vec<SocketAddr> {
+        self.hosts
+        .iter()
+        .flat_map(|addr| { 
+            let full_addr = format!("{}:{}", addr, port);
+            full_addr.to_socket_addrs().ok()
+        })
+        .flatten()
+        .collect::<Vec<SocketAddr>>()
     }
 }
-
 
 fn main() {
 
@@ -34,31 +51,32 @@ fn main() {
         let num_threads = args.number_of_threads; // Set the desired number of threads
 
         // Build a thread pool with a specific number of threads
-        let pool = ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
+        let pool = ThreadPoolBuilder::new()
+                                                .num_threads(num_threads)
+                                                .build()
+                                                .unwrap_or_else(|e| {
+                                                    eprintln!("Failed to create a thread pool: {}", e);
+                                                    std::process::exit(1);
+                                                });
         pool.install(|| {
             (1..=65535).into_par_iter().for_each(|i| {
-                scan(format!("{}:{}", args.host, i))
+                scan_socket_addresses(args.get_socket_addrs_with_port(i));
             });
         });
     } else {
-        scan(args.url());
+        scan_socket_addresses(args.get_socket_addrs());
     }
 }
 
-fn scan(address: String) {
-    let addrs = match address.to_socket_addrs() {
-        Ok(addrs) => addrs,
-        Err(e) => {
-            println!("Failed to resolve address {}: {}", address, e);
-            return;
-        }
-    };
+fn scan_socket_addresses<I>(socket_addresses: I)
+where
+    I: IntoIterator<Item = SocketAddr>,
+{
+    let timeout = Duration::new(1, 0);
 
-    for addr in addrs {
-        let timeout = Duration::new(0, 1000);
+    for addr in socket_addresses {
         if let Ok(_stream) = TcpStream::connect_timeout(&addr, timeout) {
             println!("{} is open", addr);
-            return;
         }
     }
 }
